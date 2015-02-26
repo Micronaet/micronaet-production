b8e868f6a21567fcc0452e792beb991d8a764126
nicothebrush <nicola.riolini@gmail.com>
2015-02-26 22:49:40 +0100

    Aggiunta videata con elenco previsioni di produzione

--- production_accounting_external/accounting.py
+++ production_accounting_external/accounting.py
@@ -66,12 +66,42 @@ class SaleOrderLine(orm.Model):
             'mrp.production', 'Production', ondelete='set null', ),
         }
 
+class SaleOrderLinePrevisional(orm.Model):
+    ''' Previsional production
+    '''
+    
+    _name = 'sale.order.line.previsional'
+            
+    _columns = {
+        'partner_id':fields.many2one(
+            'res.partner', 'Customer', required=False),
+        'deadline': fields.datetime('Deadline'), 
+        'note': fields.text('Note'),        
+        'product_uom_qty': fields.float('Quantity', digits=(16, 2)),
+        'updated': fields.boolean('Updated', 
+            help='Manually updated on accounting program'),
+        'mrp_id': fields.many2one(
+            'mrp.production', 'Production', ondelete='cascade', ),
+        }        
+
 class MrpProduction(orm.Model):
     ''' Add extra field to manage connection with accounting
     '''
     
     _inherit = 'mrp.production'
     
+    # TODO 
+    #def get_extra_production(self, cr, uid, ids, context=None):
+    #    ''' Serch production with that line
+    #    '''
+    #    res = {}
+    #    vote_ids = self.pool.get('sale.order.line').browse(
+    #        cr, uid, ids, context=context)
+    #    for v in vote_ids:
+    #    res[v.idea_id.id] = True # Store the idea identifiers in a set
+    #    return res.keys()
+
+
     def _get_totals(self, cr, uid, ids, fields=None, args=None, context=None):
         ''' Calculate all totals 
             oc_qty = sum (qty for all line)
@@ -86,6 +116,7 @@ class MrpProduction(orm.Model):
             res[order.id][
                 'extra_qty'] = order.product_qty - res[order.id]['oc_qty']
             res[order.id]['error_qty'] = res[order.id]['extra_qty'] < 0.0
+            res[order.id]['has_extra_qty'] = res[order.id]['extra_qty'] > 0.0
         return res    
     
     _columns = {
@@ -95,6 +126,11 @@ class MrpProduction(orm.Model):
         'extra_qty': fields.function(
             _get_totals, method=True, type='float', 
             string='Extra qty', store=False, readonly=True, multi=True),
+        'has_extra_qty': fields.function(
+            _get_totals, method=True, type='boolean', string='Has extra', 
+            store=True,
+            #{'sale.order.line':(_get_extra_production,['product_uom_qty'],10)}, 
+            readonly=True, multi=True),
         'error_qty': fields.function(
             _get_totals, method=True, type='boolean', 
             string='Total error', store=False, readonly=True, multi=True),
@@ -102,5 +138,8 @@ class MrpProduction(orm.Model):
         # TODO there's extra bool?
         'order_line_ids': fields.one2many(
             'sale.order.line', 'mrp_id', 'Order line'),
+        'previsional_line_ids': fields.one2many(
+            'sale.order.line.previsional', 'mrp_id', 'Previsional order'),
         }
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
