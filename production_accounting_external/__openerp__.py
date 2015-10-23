3d086d86a866bd954b77b7b49da6fa4f3b927db0
nicothebrush <nicola.riolini@gmail.com>
2015-10-23 11:37:38 +0200

    Add dependency for hide column in order

--- production_accounting_external/__openerp__.py
+++ production_accounting_external/__openerp__.py
@@ -44,8 +44,10 @@
         'sql_sale_order',
         'production_order_mandatory',
         'production_workhour',
+        
         # Family in sale.order.line:
         'production_order_line_family', # Micronaet/micronaet-mx8.git 
+        'order_line_view', # Micronaet/micronaet-mx8.git
         ],
     'init_xml': [],
     'demo': [],
