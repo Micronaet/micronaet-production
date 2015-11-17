0a5ad9840ae7302852697e21a51f56bb7f1599a6
nicothebrush <nicola.riolini@gmail.com>
2015-11-17 12:35:36 +0100

    manage in 2 step (update for append production)

--- production_accounting_external/wizard/create_production.py
+++ production_accounting_external/wizard/create_production.py
@@ -417,16 +417,18 @@ class CreateMrpProductionWizard(orm.TransientModel):
 
         else: # 'append'
             p_id = context['mrp_data']['append_production_id']
-            production_pool.write(               
-                cr, uid, p_id{
-                    'date_planned': context['mrp_data']['schedule_from_date'],
-                    'mrp_id': [(6, 0, context.get("active_ids", []))]
-                    }. context=context)
+
             # Add sale order line to production:
-            #self.pool.get('sale.order.line').write(
-            #    cr, uid, context.get("active_ids", []), {
-            #        'mrp_id': p_id,
-            #        }, context=context)
+            self.pool.get('sale.order.line').write(
+                cr, uid, context.get("active_ids", []), {
+                    'mrp_id': p_id,
+                    }, context=context)
+            # Udate start date:        
+            production_pool.write(               
+                cr, uid, p_id, {
+                    'date_planned': context['mrp_data']['schedule_from_date'],
+                    #'order_line_ids': [(6, 0, context.get("active_ids", []))],
+                    }, context=context)
 
         # Reforce total from sale order line:
         production_pool.recompute_total_from_sol(
