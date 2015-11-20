3cbbc498acc8360c0b0ab0d080b8d027c7abe28d
nicothebrush <nicola.riolini@gmail.com>
2015-11-20 20:36:42 +0100

    [ADD] production_accounting_external_startup: start module for manage all order lines that are yet procuder and delivered

---
+++ production_accounting_external_startup/__openerp__.py
@@ -0,0 +1,48 @@
+###############################################################################
+#
+#    Copyright (C) 2001-2014 Micronaet SRL (<http://www.micronaet.it>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    'name': 'Production accounting external startup',
+    'version': '0.1',
+    'category': 'Startup',
+    'description': '''  
+        This module create a fax production where put all family lines that
+        are yet produced in initial phase.
+        Production order are minimal and has only lines that are produced or
+        delivered, information readed from accounting code.
+        ''',
+    'author': 'Micronaet S.r.l. - Nicola Riolini',
+    'website': 'http://www.micronaet.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'production_accounting_external',
+        'accounting_statistic_order', # For get text file with order closed        
+        ],
+    'init_xml': [],
+    'demo': [],
+    'data': [
+        #'security/ir.model.access.csv',    
+        'startup_views.xml', # after wizard 
+        'scheduler.xml',     
+        ],
+    'active': False,
+    'installable': True,
+    'auto_install': False,
+    }
---
+++ production_accounting_external_startup/startup.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Copyright (C) 2001-2014 Micronaet SRL (<http://www.micronaet.it>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+import os
+import sys
+import logging
+import openerp
+import openerp.netsvc as netsvc
+import openerp.addons.decimal_precision as dp
+from openerp.osv import fields, osv, expression, orm
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from openerp import SUPERUSER_ID, api
+from openerp import tools
+from openerp.tools.translate import _
+from openerp.tools.float_utils import float_round as round
+from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT, 
+    DEFAULT_SERVER_DATETIME_FORMAT, 
+    DATETIME_FORMATS_MAP, 
+    float_compare)
+
+
+_logger = logging.getLogger(__name__)
+
+class SaleOrder(orm.Model):
+    ''' Add extra field for sync order
+    '''
+    _name = 'sale.order'
+    
+    # -----------------
+    # Scheduled action:
+    # -----------------
+    def scheduled_import_order_and_sync(self, cr, uid, 
+            csv_file='~/etl/ocdetoerp1.csv', separator=';', header=0, 
+            verbose=100, context=None)
+        ''' Sync current sale.order improted from accounting with same sale
+            order of an old procedure for manage delivery and transport
+            (this order has note and B extra information
+            # Note: All file import information now is not managed, use 
+                    importation header and lines (after import directly form 
+                    here for by pass old procedure                    
+        '''
+        # 1. Import all csv file in temporary order object
+        
+        # 2. Read all importation order and chech header cases:
+        #    3 Case: Present fake obj, presente both, present real obj
+        
+        # 3. Sync line in middle case
+        # > Create fake production order and put there all line 
+        
+        return True
+        
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
