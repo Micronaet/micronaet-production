1c917540e166a7d14441ef4b52523b36ee669e66
nicothebrush <nicola.riolini@gmail.com>
2016-03-15 10:43:12 +0100

    end view with sol data

--- production_accounting_external_stats/stats.py
+++ production_accounting_external_stats/stats.py
@@ -57,17 +57,23 @@ class MrpProductionStatMixed(osv.osv):
     '''
     _name = 'mrp.production.stats.mixed'
     _description = 'MRP stats mixed'
+    _order = 'workcenter_id,date_planned'
     _auto = False
     
     _columns = {
-        'name': fields.char('MRP name', required=True, readonly=True),
+        # mrp.production.workcenter.line:
+        'name': fields.char('MRP name', readonly=True),
+        'date_planned': fields.date('Date planned', readonly=True),
         'production_id': fields.many2one(
             'mrp.production', 'Production', readonly=True), 
         'workcenter_id': fields.many2one(
             'mrp.workcenter', 'Line', readonly=True), 
-        'qty': fields.float('Total q.', readonly=True),
         'lavoration_qty': fields.float('Lavoration q.', readonly=True),
-
+        
+        # sale.order.line:
+        'todo_qty': fields.float('Total q.*', readonly=True),
+        'maked_qty': fields.float('Done q.*', readonly=True),
+        'remain_qty': fields.float('Remain q.*', readonly=True),
     
         #'name': fields.char('Year', required=False, readonly=True),
         #'month':fields.selection([('01','January'), ('02','February'), ('03','March'), ('04','April'), ('05','May'), ('06','June'),
@@ -91,59 +97,30 @@ class MrpProductionStatMixed(osv.osv):
                     wl.name as name,
                     wl.production_id as production_id,
                     wl.workcenter_id as workcenter_id,
-                    wl.qty as qty,
-                    wl.lavoration_qty as lavoration_qty
+                    wl.lavoration_qty as lavoration_qty,
+                    wl.date_planned as date_planned,
+                    
+                    sol.todo_qty as todo_qty,
+                    sol.maked_qty as maked_qty,
+                    
+                    (sol.todo_qty - sol.maked_qty) as remain_qty
                 FROM
-                    mrp_production_workcenter_line wl    
+                    mrp_production_workcenter_line wl
+                    LEFT JOIN 
+                    (
+                        SELECT 
+                            mrp_id, 
+                            sum(product_uom_qty) todo_qty,
+                            sum(product_uom_maked_sync_qty) maked_qty
+                        FROM sale_order_line
+                        GROUP BY
+                            mrp_id
+                    ) sol
+                    ON (wl.production_id = sol.mrp_id)
+                    
                 WHERE 
                     wl.state != 'cancel'
                 )""")
-        
-        TODO_tmp = """
-            create or replace view report_intrastat as (
-                select
-                    to_char(inv.create_date, 'YYYY') as name,
-                    to_char(inv.create_date, 'MM') as month,
-                    min(inv_line.id) as id,
-                    intrastat.id as intrastat_id,
-                    upper(inv_country.code) as code,
-                    sum(case when inv_line.price_unit is not null
-                            then inv_line.price_unit * inv_line.quantity
-                            else 0
-                        end) as value,
-                    sum(
-                        case when uom.category_id != puom.category_id then (pt.weight_net * inv_line.quantity)
-                        else (pt.weight_net * inv_line.quantity * uom.factor) end
-                    ) as weight,
-                    sum(
-                        case when uom.category_id != puom.category_id then inv_line.quantity
-                        else (inv_line.quantity * uom.factor) end
-                    ) as supply_units,
-
-                    inv.currency_id as currency_id,
-                    inv.number as ref,
-                    case when inv.type in ('out_invoice','in_refund')
-                        then 'export'
-                        else 'import'
-                        end as type
-                from
-                    account_invoice inv
-                    left join account_invoice_line inv_line on inv_line.invoice_id=inv.id
-                    left join (product_template pt
-                        left join product_product pp on (pp.product_tmpl_id = pt.id))
-                    on (inv_line.product_id = pp.id)
-                    left join product_uom uom on uom.id=inv_line.uos_id
-                    left join product_uom puom on puom.id = pt.uom_id
-                    left join report_intrastat_code intrastat on pt.intrastat_id = intrastat.id
-                    left join (res_partner inv_address
-                        left join res_country inv_country on (inv_country.id = inv_address.country_id))
-                    on (inv_address.id = inv.partner_id)
-                where
-                    inv.state in ('open','paid')
-                    and inv_line.product_id is not null
-                    and inv_country.intrastat=true
-                group by to_char(inv.create_date, 'YYYY'), to_char(inv.create_date, 'MM'),intrastat.id,inv.type,pt.intrastat_id, inv_country.code,inv.number,  inv.currency_id
-            )"""
 
 class MrpProduction(orm.Model):
     ''' Statistic data
