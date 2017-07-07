59270fc3d5432fb7a74ddf8fac52a186d87fda79
nicothebrush <nicola.riolini@gmail.com>
2017-07-07 12:20:57 +0200

    fast git

--- fsc_management/fsc.py
+++ fsc_management/fsc.py
@@ -87,8 +87,7 @@ class ResCompany(orm.Model):
         # ---------------------------------------------------------------------
         query = '''
             UPDATE product_product 
-            SET %s_certified_id=null 
-            WHERE %s_certified_id>0;
+            SET %s_certified_id=null;
             ''' % (mode, mode)
         cr.execute(query)
         _logger.info('Clean previous selection: \n%s' % query)
