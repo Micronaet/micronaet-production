f14522297b9071d25ffd40658868dbc2e9675c4e
nicothebrush <nicola.riolini@gmail.com>
2017-07-25 15:33:01 +0200

    add report for BF

--- fsc_management/wizard/export_invoice.py
+++ fsc_management/wizard/export_invoice.py
@@ -47,25 +47,26 @@ class ExportXlsxFscReportWizard(orm.TransientModel):
     '''
     _name = 'export.xlsx.fsc.report.wizard'
 
+    # ---------------------------------------------------------------------
+    # Utility:
+    # ---------------------------------------------------------------------
+    def xls_write_row(self, WS, row, row_data, format_cell):
+        ''' Print line in XLS file            
+        '''
+        ''' Write line in excel file
+        '''
+        col = 0
+        for item in row_data:
+            WS.write(row, col, item, format_cell)
+            col += 1
+        return True
+
     # -------------------------------------------------------------------------
     # Wizard button event:
     # -------------------------------------------------------------------------    
     def action_print(self, cr, uid, ids, context=None):
         ''' Event for button done
         '''
-        # ---------------------------------------------------------------------
-        # Utility:
-        # ---------------------------------------------------------------------
-        def xls_write_row(WS, row, row_data, format_cell):
-            ''' Print line in XLS file            
-            '''
-            ''' Write line in excel file
-            '''
-            col = 0
-            for item in row_data:
-                WS.write(row, col, item, format_cell)
-                col += 1
-            return True
 
         if context is None: 
             context = {}
@@ -139,8 +140,8 @@ class ExportXlsxFscReportWizard(orm.TransientModel):
             WS.set_column(10, 10, 10)
         
         # Export Header:
-        xls_write_row(WS_fsc, 0, header, format_title)        
-        xls_write_row(WS_pefc, 0, header, format_title)        
+        self.xls_write_row(WS_fsc, 0, header, format_title)        
+        self.xls_write_row(WS_pefc, 0, header, format_title)        
         
         # Export data:
         order = 'number'
@@ -181,11 +182,11 @@ class ExportXlsxFscReportWizard(orm.TransientModel):
                 if fsc:
                     i_fsc += 1
                     data[5] = product.fsc_certified_id.name
-                    xls_write_row(WS_fsc, i_fsc, data, format_text)
+                    self.xls_write_row(WS_fsc, i_fsc, data, format_text)
                 else: # pefc
                     i_pefc += 1
                     data[5] = product.pefc_certified_id.name
-                    xls_write_row(WS_pefc, i_pefc, data, format_text)
+                    self.xls_write_row(WS_pefc, i_pefc, data, format_text)
 
         _logger.info('End FIDO invoice export on %s' % xls_filename)
         WB.close()
@@ -198,7 +199,152 @@ class ExportXlsxFscReportWizard(orm.TransientModel):
             'type': 'binary',
             'datas': b64,
             'partner_id': 1,
-            'res_model':'res.partner',
+            'res_model': 'res.partner',
+            'res_id': 1,
+            }, context=context)
+        
+        return {
+            'type' : 'ir.actions.act_url',
+            'url': '/web/binary/saveas?model=ir.attachment&field=datas&'
+                'filename_field=datas_fname&id=%s' % attachment_id,
+            'target': 'self',
+            }   
+
+    def action_print_bf(self, cr, uid, ids, context=None):
+        ''' Event for button done for BF
+        '''
+        if context is None: 
+            context = {}
+        
+        # Pool used:
+        pick_pool = self.pool.get('stock.picking')
+        
+        # Read parameters:
+        wiz_browse = self.browse(cr, uid, ids, context=context)[0]
+        partner_id = wiz_browse.partner_id.id
+        from_date = wiz_browse.from_date
+        to_date = wiz_browse.to_date
+
+        # ---------------------------------------------------------------------
+        # Export XLSX file:
+        # ---------------------------------------------------------------------
+        xls_filename = '/tmp/fsc_pefc_report_bf.xlsx'
+        _logger.info('Start FSC and PEFC invoice export BF: %s' % xls_filename)
+        
+        # Open file and write header
+        WB = xlsxwriter.Workbook(xls_filename)
+        WS_fsc = WB.add_worksheet(_('FSC'))
+        WS_pefc = WB.add_worksheet(_('PEFC'))
+
+        # Format:
+        format_title = WB.add_format({
+            'bold': True, 
+            'font_color': 'black',
+            'font_name': 'Arial',
+            'font_size': 10,
+            'align': 'center',
+            'valign': 'vcenter',
+            'bg_color': 'gray',
+            'border': 1,
+            'text_wrap': True,
+            })
+
+        format_text = WB.add_format({
+            'font_name': 'Arial',
+            'font_size': 9,
+            #'align': 'right',
+            #'bg_color': 'c1e7b3',
+            'border': 1,
+            #'num_format': '0.00',
+            })        
+        
+        header = [
+            _('Cliente'),
+            _('BF'),
+            _('Data'),
+            _('Prodotto'),
+            _('Nome'),
+            _('Certif.'),
+            _('Q.'),
+            _('Price'),
+            _('Discount'),            
+            _('Subtotal'),            
+           ]
+
+        # Column dimension:
+        for WS in (WS_fsc, WS_pefc):
+            WS.set_column(0, 0, 40)
+            WS.set_column(1, 1, 12)
+            WS.set_column(2, 2, 8)
+            WS.set_column(3, 3, 11)
+            WS.set_column(4, 4, 35)
+            WS.set_column(6, 6, 10)
+            WS.set_column(7, 7, 10)
+            WS.set_column(8, 8, 10)
+            WS.set_column(9, 9, 10)
+            WS.set_column(10, 10, 10)
+        
+        # Export Header:
+        self.xls_write_row(WS_fsc, 0, header, format_title)        
+        self.xls_write_row(WS_pefc, 0, header, format_title)        
+        
+        # Export data:
+        order = 'number'
+        domain = [('state', 'in', ('done', ))]
+        if partner_id:
+            domain.append(('partner_id', '=', partner_id))
+        if from_date:
+            domain.append(('date', '>=', from_date))
+        if to_date:     
+            domain.append(('date', '<=', to_date))
+
+        pick_ids = pick_pool.search(
+            cr, uid, domain, order=order, context=context)        
+        i_fsc = 0
+        i_pefc = 0
+        for pick in pick_pool.browse(
+                cr, uid, pick_ids, context=context):
+            for line in pick.move_lines:
+                # Check FSC or PEFC
+                product = line.product_id
+                fsc = product.fsc_certified_id
+                pefc = product.pefc_certified_id
+                if not fsc and not pefc:
+                    continue
+                    
+                data = [
+                    pick.partner_id.name,
+                    pick.name,
+                    pick.date, 
+                    product.default_code,
+                    product.name,
+                    False,
+                    line.product_uom_qty,
+                    '', #line.price_unit,
+                    '',#line.multi_discount_rates or '',
+                    '',#line.price_subtotal,
+                    ]
+                if fsc:
+                    i_fsc += 1
+                    data[5] = product.fsc_certified_id.name
+                    self.xls_write_row(WS_fsc, i_fsc, data, format_text)
+                else: # pefc
+                    i_pefc += 1
+                    data[5] = product.pefc_certified_id.name
+                    self.xls_write_row(WS_pefc, i_pefc, data, format_text)
+
+        _logger.info('End FIDO BF export on %s' % xls_filename)
+        WB.close()
+
+        attachment_pool = self.pool.get('ir.attachment')
+        b64 = open(xls_filename, 'rb').read().encode('base64')
+        attachment_id = attachment_pool.create(cr, uid, {
+            'name': 'FIDO BF report',
+            'datas_fname': 'fsc_pefc_invoice_report_bf.xlsx',
+            'type': 'binary',
+            'datas': b64,
+            'partner_id': 1,
+            'res_model': 'res.partner',
             'res_id': 1,
             }, context=context)
         
