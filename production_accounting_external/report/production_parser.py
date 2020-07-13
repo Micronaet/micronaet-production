4a9ab7bb36caddfa941bde3e2b948d775dfef4e2
nicothebrush <nicola.riolini@gmail.com>
2020-07-13 11:41:13 +0200

    generic bugfix (fast message)

--- production_accounting_external/report/production_parser.py
+++ production_accounting_external/report/production_parser.py
@@ -79,9 +79,9 @@ class Parser(report_sxw.rml_parse):
         components = self.product_components.get(product, {})
         for component in components:
             qty = components[component]
-            res = '\n > %s q.%s' % (
+            res = '\n > %s q. %s' % (
                 component.default_code or '?',
-                qty * remain,
+                qty * remain if qty else '/',
             )
         return res
 
