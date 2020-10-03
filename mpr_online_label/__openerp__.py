1aead4031add34b82282f201f04aa4f42b75348e
nicothebrush <nicola.riolini@gmail.com>
2020-10-03 10:40:46 +0200

    [ADD] MRP online label: Manage label printing online

---
+++ mpr_online_label/__openerp__.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# ODOO (ex OpenERP)
+# Open Source Management Solution
+# Copyright (C) 2001-2015 Micronaet S.r.l. (<https://micronaet.com>)
+# Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    'name': 'MRP online label',
+    'version': '0.1',
+    'category': 'MRP',
+    'description': '''        
+        Manage Label on MRP line
+        ''',
+    'author': 'Micronaet S.r.l. - Nicola Riolini',
+    'website': 'http://www.micronaet.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'mrp',
+        'production_accounting_external',  # MRP Management
+        'production_accounting_external_stats',  # Statistics
+        'production_product_sequence',  # Sorting procedure
+        'bom_dynamic_structured',  # BOM management
+        'label_system_mrp',
+        # 'production_product_child_move',  # Move operation on production
+        'production_forecast_order',  # Forecast management (extra production
+        # 'lavoration_cl_sl',  # Manage job
+        'note_system_management',  # Note management
+        ],
+    'init_xml': [],
+    'demo': [],
+    'data': [
+        'security/mrp_label_group.xml',
+
+        'label_view.xml',
+        ],
+    'active': False,
+    'installable': True,
+    'auto_install': False,
+    }
