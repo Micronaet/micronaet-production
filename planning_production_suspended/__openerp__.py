d381dbce648de192995120477bf7cee1c9feb6e7
nicothebrush <nicola.riolini@gmail.com>
2020-11-17 22:52:49 +0100

    [ADD] planning_production_suspended: move part in new module to not generate circ. reference

---
+++ planning_production_suspended/__openerp__.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# ODOO (ex OpenERP) 
+# Open Source Management Solution
+# Copyright (C) 2001-2015 Micronaet S.r.l. (<https://micronaet.com>)
+# Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    'name': 'Suspended MRP',
+    'version': '0.1',
+    'category': 'MRP',
+    'description': '''         
+        Add view for manage suspended production
+        ''',
+    'author': 'Micronaet S.r.l. - Nicola Riolini',
+    'website': 'http://www.micronaet.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'production_accounting_external',
+        'delivery_todo_report',  # For suspended field        
+        ],
+    'init_xml': [],
+    'demo': [],
+    'data': [
+        'suspended_mrp_view.xml',
+        ],
+    'active': False,
+    'installable': True,
+    'auto_install': False,
+    }
